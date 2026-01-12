043cf67f8f3ef45f87f02ed0b6ac3b82d1a0d367
Michael Weiser <michael.weiser@eviden.com>
2026-01-12 16:34:40 +0100

    Handle preformatted raw devices for mounts

--- specs/default/chef/site-cookbooks/local_node/recipes/permanent_mounts.rb
+++ specs/default/chef/site-cookbooks/local_node/recipes/permanent_mounts.rb
@@ -8,19 +8,29 @@ mounts = node[:cyclecloud][:mounts].sort_by { |_k, v| v[:order] || 1000 }
 mounts.each_with_index do |(name, mp), _index|
 	next unless CVolume.device_mountpoint?(mp)
 
+	# in Chef 11 node attributes are immutable, so let's convert this to a hash
+	mp = ::Hash[mp.map { |(k, v)| [k.to_sym, v] }]
+
 	next if mp[:disabled] == true
 
-	Chef::Log.debug("Enable mountpoint #{name} at #{mp[:mountpoint]} order #{mp[:order] || 1000}")
+	mp[:mountpoint] = "/media/#{name}" if mp[:mountpoint].nil?
 
-	# Determine if encrypted, and what the default device_name should be
-	lv_name = mp[:lv_name] || 'lv0'
-	vg_name = mp[:vg_name] || "vg_cyclecloud_#{name}"
-	if (mp[:encryption].nil? || mp[:encryption].empty?) && node[:cyclecloud][:mount_defaults][:encryption][:bits].nil?
-		device_name = "/dev/mapper/#{vg_name}-#{lv_name}"
+	device = node.run_state[:mounts][name][:device]
+	if CVolume.formatted?(device)
+		device_name = device
 	else
-		crypt_name = mp[:encryption][:name] || "cyclecloud_crypt_#{name}"
-		device_name = "/dev/mapper/#{crypt_name}"
+		# Determine if encrypted, and what the default device_name should be
+		lv_name = mp[:lv_name] || 'lv0'
+		vg_name = mp[:vg_name] || "vg_cyclecloud_#{name}"
+		if (mp[:encryption].nil? || mp[:encryption].empty?) && node[:cyclecloud][:mount_defaults][:encryption][:bits].nil?
+			device_name = "/dev/mapper/#{vg_name}-#{lv_name}"
+		else
+			crypt_name = mp[:encryption][:name] || "cyclecloud_crypt_#{name}"
+			device_name = "/dev/mapper/#{crypt_name}"
+		end
 	end
+
+	Chef::Log.debug("Enable mountpoint #{name} at #{mp[:mountpoint]} of device #{device_name} order #{mp[:order] || 1000}")
 
 	# Enable mount at mp[:mountpoint]
 	mount mp[:mountpoint] do
