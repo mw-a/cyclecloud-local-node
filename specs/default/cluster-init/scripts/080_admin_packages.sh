e9c8c4209fb14b9e624bb76cfd264b923b823aa2
Michael Weiser <michael.weiser@gmx.de>
2026-02-05 10:37:13 +0100

    Switch vim package name to avoid senseless run of dnf

--- specs/default/cluster-init/scripts/080_admin_packages.sh
+++ specs/default/cluster-init/scripts/080_admin_packages.sh
@@ -4,6 +4,6 @@ set -e
 # screen is in epel nowadays
 rpm -q epel-release >/dev/null 2>&1 || dnf install -y epel-release
 
-pkgs="tmux screen vim"
+pkgs="tmux screen vim-enhanced"
 pkgcount=$(echo $pkgs | wc -w)
 [ "$(rpm -qa $pkgs | wc -l )" -eq $pkgcount ] || dnf install -y $pkgs
